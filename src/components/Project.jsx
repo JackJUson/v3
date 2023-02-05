16ed43cfc285922317dbde5fbfdc8866a620d1a5
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 20:41:54 +1100

    Add project data and links over div

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import { CiFolderOn } from "react-icons/ci";
 import { FiExternalLink, FiGithub } from "react-icons/fi";
 
-function Project() {
+function Project({ project }) {
   return (
     <div
-      className="shadow-sm shadow-[#040c16] hover:-translate-y-2 duration-200 
-      hover:font-bold text-[#a8b2d1] bg-[#112240] rounded-[3px]"
+      className="shadow-sm shadow-[#040c16] hover:-translate-y-1.5 duration-300 ease-in-out
+      group text-[#a8b2d1] bg-[#112240] rounded-sm py-3 px-1"
     >
-      <div className="flex justify-between items-center m-4">
+      <div className="flex justify-between items-center m-6">
         <CiFolderOn size={40} className="text-[#64ffda]" />
         <div className="flex items-center">
           <a
-            href=""
+            href={project.github}
             target="_blank"
             className="mr-6 hover:text-[#64ffda] duration-200"
           >
             <FiGithub size={20} />
           </a>
           <a
-            href=""
+            href={project.live}
             target="_blank"
             className="hover:text-[#64ffda] duration-200"
           >
@@ -27,17 +27,21 @@ function Project() {
           </a>
         </div>
       </div>
-      <a href="" className="my-4">
-        Project Title
-      </a>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum optio
-        corrupti totam ducimus deserunt perferendis?
-      </p>
-      <ul className="flex">
-        <li>Tag</li>
-        <li>Tag</li>
-        <li>Tag</li>
+      <div className="flex flex-col text-left m-6 min-h-[150px]">
+        <a
+          href={project.live}
+          className="group-hover:text-[#64ffda] font-bold text-[#ccd6f6] text-xl"
+        >
+          {project.title}
+        </a>
+        <p className="text-sm my-4 pr-4">{project.description}</p>
+      </div>
+      <ul className="flex m-6 w-1/2 justify-between text-xs opacity-70">
+        {project.techs.map((tech, techIndex) => (
+          <li className="cursor-default" key={techIndex}>
+            {tech}
+          </li>
+        ))}
       </ul>
     </div>
   );
