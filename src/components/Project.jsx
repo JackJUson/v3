0541a77a49d2bec71f7ffd22d0648f3d28bf558a
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 17:45:26 +1100

    Add individual project component

---
+++ src/components/Project.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Project() {
+  return (
+    <div
+      className="shadow-md shadow-[#040c16] hover:-translate-y-2 duration-200 hover:font-bold 
+    hover:text-[#64ffda] text-[#a8b2d1] bg-gray-500"
+    >
+      <a href="#">Icon</a>
+      <p className="my-4">Project Title~~~</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum optio
+        corrupti totam ducimus deserunt perferendis? Molestiae architecto in ve
+        ro magni, maiores quas perspiciatis, asperiores, veniam quidem nostr
+      </p>
+      <ul className="flex">
+        <li>Tag</li>
+        <li>Tag</li>
+        <li>Tag</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Project;
