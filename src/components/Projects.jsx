daeb7da3d3d2af40fd22bb565aa8c4f9ea8da7cd
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 20:42:44 +1100

    Add project component for each projects object

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import Project from "./Project";
+import { projects } from "../data/projects";
 
 function Projects() {
   return (
     <div name="projects" className="w-full h-full bg-[#0a192f] text-center">
-      <div className="min-h-[900px] max-w-[80%] w-[1000px] mx-auto text-center flex flex-col justify-center items-center">
+      <div className="min-h-[1200px] max-w-[80%] w-[1000px] mx-auto text-center flex flex-col justify-center items-center">
         <p className="text-3xl font-bold text-[#ccd6f6] mb-4">
           Other Noteworthy Projects
         </p>
         <a href="#" className="text-[#64ffda]">
           View the archive
         </a>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center pt-8">
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center py-8">
+          {projects.map((project, index) => (
+            <Project project={project} key={index} />
+          ))}
         </div>
         <button
           className="border-[#ccd6f6] text-[#ccd6f6] group border-2 px-6 py-3 my-11 flex items-center rounded
