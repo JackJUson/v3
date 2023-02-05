c8e03af2579bd135c0169f09d7c4eed2fefbd236
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 22:31:44 +1100

    Initialize aos animation globally

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Social from "./components/Social";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 
 function App() {
+  AOS.init({ duration: 600, easing: "ease-out", once: true });
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <Social />
       <Home />
       <About />
