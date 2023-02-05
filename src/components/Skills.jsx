ed90c7d3c0eb0579b7131761f5a9e967fb90e790
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 17:41:05 +1100

    Change skill component to contain developer experience

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,44 +1,47 @@
 import React from "react";
 
-import HTML from "../assets/html.png";
-import CSS from "../assets/css.png";
-import JavaScript from "../assets/javascript.png";
-import ReactImg from "../assets/react.png";
-import Node from "../assets/node-js.png";
-import FireBase from "../assets/firebase.png";
-import Tailwind from "../assets/tailwind.png";
-import TypeScript from "../assets/typescript.png";
-import Skill from "./Skill";
-
 function Skills() {
   return (
-    <div name="skills" className="w-full h-full bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-full bg-[#0a192f]">
       {/* Container */}
       <div
-        className="lg:h-[50vh] min-h-[700px] max-w-[80%] w-[1000px] 
-      mx-auto flex flex-col justify-center h-full px-11 py-[4rem]"
-        data-aos="fade-up"
-        data-aos-easing="ease-out"
-        data-aos-delay="500"
-        data-aos-duration="600"
-        data-aos-once="true"
+        className="min-h-[600px] max-w-[80%] w-[1000px] 
+      mx-auto flex justify-center items-center"
       >
-        <div>
-          <p className="text-4xl font-bold inline border-b-4 border-[#64ffda]">
-            Experience
+        <div className="w-[100%] md:w-[70%]">
+          <p className="text-3xl font-bold inline border-b-4 border-[#64ffda] text-gray-300">
+            Where I’ve Worked
           </p>
-          <p className="py-4">These are my favourite technologies!</p>
-        </div>
-
-        <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-4 text-center pt-8">
-          <Skill image={HTML} title={"HTML"} />
-          <Skill image={CSS} title={"CSS"} />
-          <Skill image={JavaScript} title={"JavaScript"} />
-          <Skill image={TypeScript} title={"TypeScript"} />
-          <Skill image={ReactImg} title={"React"} />
-          <Skill image={FireBase} title={"FireBase"} />
-          <Skill image={Tailwind} title={"Tailwind"} />
-          <Skill image={Node} title={"Node"} />
+          <div className="flex mt-11 text-[#64ffda]">
+            <ul className="text-sm">
+              <li className="p-2">Frontend Simplified</li>
+            </ul>
+            <div className="flex flex-col ml-4">
+              <div className="text-gray-300 mb-4">
+                <h4 className="font-bold text-lg">
+                  Frontend Developer{" "}
+                  <a href="https://frontendsimplified.com/" className="text-[#64ffda]">
+                    &nbsp;@ Frontend Simplified
+                  </a>
+                </h4>
+                <h5 className="opacity-70 text-sm">Sep 2022 - Present</h5>
+              </div>
+              <ul className="relative flex flex-col justify-evenly h-full text-[#a8b2d1]">
+                <li className="before-arrow pl-7 my-2 text-[15px]">
+                  Support students and developers using HTML, CSS, Flexbox,
+                  JavaScript and React best practises.
+                </li>
+                <li className="before-arrow pl-7 my-2 text-[15px]">
+                  Utilized debugging best practises using Google Chrome inspect
+                  element tooling.
+                </li>
+                <li className="before-arrow pl-7 my-2 text-[15px]">
+                  Simplified difficult programming concepts including responsive
+                  website development, Promises and API requests.
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
