19d8a82448c0bd808a5140a86e9a44d22f7a7dd4
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 22:10:32 +1100

    Remove aos initiation from app.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
 import Social from "./components/Social";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 
 function App() {
-  AOS.init();
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Social />
       <Home />
       <About />
