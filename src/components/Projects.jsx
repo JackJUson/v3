e73109cfe4a825c927bfd53cd59ffac1aea76a27
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 17:44:59 +1100

    Add projects component containing remaining projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Project from "./Project";
+
+function Projects() {
+  return (
+    <div name="projects" className="w-full h-full bg-[#0a192f] text-center">
+      <div className="min-h-[900px] max-w-[80%] w-[1000px] mx-auto text-center flex flex-col justify-center items-center">
+        <p className="text-3xl font-bold text-gray-300 mb-4">
+          Other Noteworthy Projects
+        </p>
+        <a href="#" className="text-[#64ffda]">
+          View the archive
+        </a>
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center pt-8">
+          <Project />
+          <Project />
+          <Project />
+          <Project />
+          <Project />
+          <Project />
+        </div>
+        <button
+          className="border-[#ccd6f6] text-[#ccd6f6] group border-2 px-6 py-3 my-11 flex items-center rounded
+          hover:bg-[#64ffda] hover:bg-opacity-40 hover:border-[#64ffda] hover:text-white duration-300"
+        >
+          Show More
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
