ffd8c8ab4c7f6818308f1755f51f95de8e812a98
Jackson <z5397791@ad.unsw.edu.au>
2023-02-05 18:07:58 +1100

    Add folder icon, github and link anchors to project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,38 @@
 import React from "react";
+import { CiFolderOn } from "react-icons/ci";
+import { FiExternalLink, FiGithub } from "react-icons/fi";
 
 function Project() {
   return (
     <div
-      className="shadow-md shadow-[#040c16] hover:-translate-y-2 duration-200 hover:font-bold 
-    hover:text-[#64ffda] text-[#a8b2d1] bg-gray-500"
+      className="shadow-sm shadow-[#040c16] hover:-translate-y-2 duration-200 
+      hover:font-bold text-[#a8b2d1] bg-[#112240] rounded-[3px]"
     >
-      <a href="#">Icon</a>
-      <p className="my-4">Project Title~~~</p>
+      <div className="flex justify-between items-center m-4">
+        <CiFolderOn size={40} className="text-[#64ffda]" />
+        <div className="flex items-center">
+          <a
+            href=""
+            target="_blank"
+            className="mr-6 hover:text-[#64ffda] duration-200"
+          >
+            <FiGithub size={20} />
+          </a>
+          <a
+            href=""
+            target="_blank"
+            className="hover:text-[#64ffda] duration-200"
+          >
+            <FiExternalLink size={20} />
+          </a>
+        </div>
+      </div>
+      <a href="" className="my-4">
+        Project Title
+      </a>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum optio
-        corrupti totam ducimus deserunt perferendis? Molestiae architecto in ve
-        ro magni, maiores quas perspiciatis, asperiores, veniam quidem nostr
+        corrupti totam ducimus deserunt perferendis?
       </p>
       <ul className="flex">
         <li>Tag</li>
