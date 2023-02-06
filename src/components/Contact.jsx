d6c5f53f695dc440c436dc6390e47ada5d1384e7
Jackson <z5397791@ad.unsw.edu.au>
2023-02-06 19:46:30 +1100

    Add animation to each element to remove bottom white space movement on mount

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,18 +6,26 @@ function Contact() {
       name="contact"
       className="w-full h-[50vh] min-h-[600px] bg-[#0a192f] text-[#8892b0] text-center"
     >
-      <div
-        className="h-full max-w-[80%] w-[1000px] mx-auto flex flex-col justify-center items-center"
-        data-aos="fade-up"
-        data-aos-delay="500"
-      >
-        <h3 className="text-[#64ffda] tracking-wider font-bold">
+      <div className="h-full max-w-[80%] w-[1000px] mx-auto flex flex-col justify-center items-center">
+        <h3
+          className="text-[#64ffda] tracking-wider font-bold"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
           What's Next?
         </h3>
-        <h1 className="text-5xl md:text-6xl text-[#ccd6f6] block font-bold my-6">
+        <h1
+          className="text-5xl md:text-6xl text-[#ccd6f6] block font-bold my-6"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
           Get In Touch
         </h1>
-        <p className="max-w-[600px] mx-8">
+        <p
+          className="max-w-[600px] mx-8"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
           I am currently looking for new opportunities so my inbox is always
           open. Whether you have a question or just want to say hi, please send
           me a message and I'll get back to you!
@@ -26,6 +34,8 @@ function Contact() {
           <button
             className="border-[#ccd6f6] text-[#ccd6f6] group border-2 px-6 py-3 my-11 flex items-center rounded
           hover:bg-[#64ffda] hover:bg-opacity-40 hover:border-[#64ffda] hover:text-white duration-300"
+            data-aos="fade-up"
+            data-aos-delay="500"
           >
             Say Hello
           </button>
