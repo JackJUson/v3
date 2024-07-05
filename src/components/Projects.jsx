a10f48d2a5f0d7434c37d6d76a64dca8a1affc09
Jackson Jung <jackson.w.jung@gmail.com>
2024-07-05 23:47:22 +1000

    Update show button in projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import Project from "./Project";
-import { projects } from "../data/projects";
+import React, { useState } from 'react';
+import Project from './Project';
+import { projects } from '../data/projects';
 
 function Projects() {
   const initialNumberOfItems = 6;
@@ -15,40 +15,60 @@ function Projects() {
   }
 
   return (
-    <div name="projects" className="w-full h-full bg-[#0a192f] text-center">
+    <div name='projects' className='w-full h-full bg-[#0a192f] text-center'>
       <div
-        className="max-w-[80%] w-[1000px] mx-auto text-center flex flex-col justify-center items-center"
-        data-aos="fade-up"
-        data-aos-delay="400"
+        className='max-w-[80%] w-[1000px] mx-auto text-center flex flex-col justify-center items-center'
+        data-aos='fade-up'
+        data-aos-delay='400'
       >
-        <p className="text-3xl font-bold text-[#ccd6f6] mb-4 mt-[7rem]">
+        <p className='text-3xl font-bold text-[#ccd6f6] mb-4 mt-[7rem]'>
           Other Noteworthy Projects
         </p>
-        <a href="#" className="text-[#64ffda]">
+        <a href='#' className='text-[#64ffda]'>
           View the archive
         </a>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center py-8">
+        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center py-8'>
           {projects.slice(0, numOfResults).map((project, index) => (
             <Project project={project} key={index} />
           ))}
         </div>
 
         {numOfResults < projects.length ? (
-          <button
-            className="border-[#ccd6f6] text-[#ccd6f6] group border-2 px-6 py-3 my-11 flex items-center rounded
-        hover:bg-[#64ffda] hover:bg-opacity-40 hover:border-[#64ffda] hover:text-white duration-300 mb-[7rem]"
-            onClick={() => showMore()}
-          >
-            Show More
-          </button>
+          <div>
+            <button onClick={() => showMore()} class='relative inline-block my-11 mb-[7rem]'>
+              <span
+                class='absolute top-0 left-0 rounded h-full px-8 py-4 border border-[#64ffda]'
+                style={{ backgroundColor: '#64ffda' }}
+                transition-all
+                duration-300
+              >
+                <span class='opacity-0 font-medium'>Show More</span>
+              </span>
+              <span
+                class='font-medium relative inline-block rounded border bg-[#0a192f] px-8 py-4 text-[#64ffda] 
+                border-[#64ffda] transition-all duration-200 ease-in-out hover:translate-x-[-4px] hover:translate-y-[-4px]'
+              >
+                Show More
+              </span>
+            </button>
+          </div>
         ) : (
-          <button
-            className="border-[#ccd6f6] text-[#ccd6f6] group border-2 px-6 py-3 my-11 flex items-center rounded
-          hover:bg-[#64ffda] hover:bg-opacity-40 hover:border-[#64ffda] hover:text-white duration-300 mb-[7rem]"
-            onClick={() => showLess()}
+          <button onClick={() => showLess()} class='relative inline-block my-11 mb-[7rem]'>
+          <span
+            class='absolute top-0 left-0 rounded h-full px-8 py-4 border border-[#64ffda]'
+            style={{ backgroundColor: '#64ffda' }}
+            transition-all
+            duration-300
+          >
+            <span class='opacity-0 font-medium'>Show Less</span>
+          </span>
+          <span
+            class='font-medium relative inline-block rounded border bg-[#0a192f] px-8 py-4 text-[#64ffda] 
+            border-[#64ffda] transition-all duration-200 ease-in-out hover:translate-x-[-4px] hover:translate-y-[-4px]'
           >
             Show Less
-          </button>
+          </span>
+        </button>
         )}
       </div>
     </div>
