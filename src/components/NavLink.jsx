9c150838c456651804c872bba586631041b30804
Jackson Jung <jackson.w.jung@gmail.com>
2024-07-06 19:45:28 +1000

    Add more horizontal margin to nav links

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Link } from "react-scroll";
+import React from 'react';
+import { Link } from 'react-scroll';
 
 function NavLink({ screen, link, name, handleClick, delay }) {
   return screen ? (
     <li
-      className="mx-4 hover:text-[#64ffda] duration-300 cursor-pointer"
-      data-aos="fade-in"
+      className='mx-6 hover:text-[#64ffda] duration-200 cursor-pointer'
+      data-aos='fade-in'
       data-aos-delay={delay}
     >
       <Link to={link} smooth={true} duration={1000}>
@@ -13,7 +13,7 @@ function NavLink({ screen, link, name, handleClick, delay }) {
       </Link>
     </li>
   ) : (
-    <li className="my-6 text-4xl hover:text-[#64ffda] duration-300 cursor-pointer">
+    <li className='my-6 text-4xl hover:text-[#64ffda] duration-200 cursor-pointer'>
       <Link onClick={handleClick} to={link} smooth={true} duration={1000}>
         {name}
       </Link>
