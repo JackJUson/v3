f5ddb5b724327267b1a74c3a497dbea9806dff90
Jackson Jung <jackson.w.jung@gmail.com>
2025-09-18 22:37:13 +1000

    Update footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,19 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
   return (
-    <div className="w-full bg-[#0a192f] text-[#a8b2d1] text-xs">
-      <div className="flex flex-col justify-center items-center py-4">
-        <p className="mb-2">
+    <div className='w-full bg-[#0a192f] text-[#a8b2d1] text-xs'>
+      <div className='flex flex-col justify-center items-center py-4'>
+        <p className='mb-2'>
           <a
-            href="https://github.com/JackJUson/v2"
-            target="_blank"
-            className="hover:text-[#64ffda] duration-200"
+            href='https://github.com/JackJUson/v3'
+            target='_blank'
+            className='hover:text-[#64ffda] duration-200'
           >
-            Built by Jackson Jung
-          </a>{" "}
-          &{" "}
-          <a
-            href="https://github.com/bchiang7/v4"
-            target="_blank"
-            className="hover:text-[#64ffda] duration-200"
-          >
-            Designed by Brittany Chiang
+            Developed by Jackson Jung
           </a>
         </p>
-        <p className="mb-2">&copy; Copyright 2024 | All Rights Reserved</p>
+        <p className='mb-2'>&copy; Copyright 2025 | All Rights Reserved</p>
       </div>
     </div>
   );
