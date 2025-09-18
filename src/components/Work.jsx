de16ffcf92deb3eba66c522161d5dda56a2cf3d4
Jackson Jung <jackson.w.jung@gmail.com>
2025-09-18 22:37:28 +1000

    Update projects section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -20,13 +20,18 @@ function Work() {
               data-aos-delay='300'
               key={index}
             >
-              <div className='hidden md:flex items-center hover:scale-105 duration-200'>
-                <a href={project.github} target='_blank'>
+              <div className='hidden md:block items-center duration-200'>
+                <a
+                  href={project.github}
+                  target='_blank'
+                  className='group block relative overflow-hidden bg-[#64ffda] transition-colors duration-300 ease-in-out'
+                >
                   <img
                     src={project.image}
                     alt='Project Image'
-                    className='w-full shadow-2xl rounded-sm object-fill'
+                    className='w-full object-fill mix-blend-multiply grayscale contrast-100 transition-all duration-300 ease-in-out group-hover:mix-blend-normal group-hover:grayscale-0'
                   />
+                  <div className='pointer-events-none absolute inset-0 bg-[#0a192f] mix-blend-screen transition-opacity duration-300 ease-in-out group-hover:opacity-0'></div>
                 </a>
               </div>
               <div className='text-left md:text-right items-start md:items-end flex flex-col justify-evenly min-h-[350px] sm:z-40'>
@@ -43,7 +48,7 @@ function Work() {
                 <p className='md:bg-[#112240] md:w-[130%] min-w-[200px] md:p-6 md:pr-9 z-40 rounded-sm shadow-xl text-[#a8b2d1]'>
                   {project.description}
                 </p>
-                <ul className='flex justify-between text-sm w-[50vw] md:w-[130%] md:min-w-[200px] lg:w-[90%] z-40 font-thin'>
+                <ul className='flex justify-between text-sm w-[50vw] md:w-[130%] md:min-w-[200px] lg:w-[90%] z-40'>
                   {project.techs.map((tech, techIndex) => (
                     <li className='cursor-default' key={techIndex}>
                       {tech}
@@ -89,9 +94,9 @@ function Work() {
                 <p className='md:bg-[#112240] md:w-[130%] min-w-[200px] md:p-6 md:pl-7 z-40 rounded-sm shadow-xl text-[#a8b2d1]'>
                   {project.description}
                 </p>
-                <ul className='flex wrap justify-between text-sm w-[50vw] md:w-[150%] md:min-w-[200px] lg:w-[120%] z-40 font-thin'>
+                <ul className='flex justify-start text-sm w-[50vw] md:w-[150%] md:min-w-[200px] lg:w-[120%] z-40'>
                   {project.techs.map((tech, techIndex) => (
-                    <li className='cursor-default margin' key={techIndex}>
+                    <li className='cursor-default mr-8' key={techIndex}>
                       {tech}
                     </li>
                   ))}
@@ -113,13 +118,18 @@ function Work() {
                   </a>
                 </div>
               </div>
-              <div className='hidden md:flex items-center hover:scale-105 duration-200 max-w-[500px]'>
-                <a href={project.github} target='_blank'>
+              <div className='hidden md:block items-center duration-200 max-w-[500px]'>
+                <a
+                  href={project.github}
+                  target='_blank'
+                  className='group block relative overflow-hidden bg-[#64ffda] transition-colors duration-300 ease-in-out'
+                >
                   <img
                     src={project.image}
                     alt='Project Image'
-                    className='w-full shadow-2xl rounded-sm'
+                    className='w-full mix-blend-multiply grayscale contrast-100 transition-all duration-300 ease-in-out group-hover:mix-blend-normal group-hover:grayscale-0'
                   />
+                  <div className='pointer-events-none absolute inset-0 bg-[#0a192f] mix-blend-screen transition-opacity duration-300 ease-in-out group-hover:opacity-0'></div>
                 </a>
               </div>
             </div>
